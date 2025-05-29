0db58b058f481965a47711349ca4958abd63f431
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2025-05-29 21:31:19 +0100

    toggleTheme

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
-import {Sun, Moon} from "lucide-react";
+import {Sun, Moon, TruckElectricIcon} from "lucide-react";
 
 export const ThemeToggle = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false)
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
-    return <button>{isDarkMode ? <Sun /> : <Moon />}</button>
+    const toggleTheme = () => {
+        if (isDarkMode) {
+            setIsDarkMode(false);
+        } else {
+            setIsDarkMode(true);
+        }
+    };
+
+    return <button onClick={toggleTheme}>{isDarkMode ? <Sun className="h-6 w-6 text-yellow-400"/> : <Moon className="h-6 w-6 text-blue-800"/>}</button>
 };
