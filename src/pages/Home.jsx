258bf1bf5b83b38378a3a127da3edb2c0767a336
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2025-06-04 22:23:58 +0100

    StarBackground

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackground } from "../components/StarBackground"
 
 export const Home = () => {
-    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+    return ( <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
     { /* Theme Toggle */}
     <ThemeToggle />
 
     { /* Background */}
+    <StarBackground />
 
     { /* Nav */ }
 
@@ -15,4 +17,5 @@ export const Home = () => {
     {/* Fotter */}
 
     </div>
-}
+    )
+};
